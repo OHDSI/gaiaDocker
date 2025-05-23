22fdad82dd8c1017b5837a1df0dfe1a8910c2158
Timothy Norris <tibben@ocf.berkeley.edu>
2025-05-23 00:48:25 -0400

    debug pgpass on postgis

--- gaia-postgis/init.sh
+++ gaia-postgis/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo gaia-db:$POSTGRES_PORT:$POSTGRES_DB:$POSTGRES_USER:$(cat $POSTGRES_PASSWORD_FILE) > ~/.pgpass
+chmod 0600 ~/.pgpass
 export GAIA_X_API_KEY=$(cat $GAIA_X_API_KEY_FILE)
 
 tcpserver 0.0.0.0 $GAIA_POSTGIS_API_PORT ./api.sh
