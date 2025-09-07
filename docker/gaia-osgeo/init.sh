9a71fcfe35c5356bc6576bfb4cf5ec4995f9fdb0
Timothy Norris <tibben@ocf.berkeley.edu>
2025-09-07 19:15:11 -0400

    feat: add USGS API key functionality for SRTM

--- docker/gaia-osgeo/init.sh
+++ docker/gaia-osgeo/init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export POSTGRES_PASSWORD=$(cat $POSTGRES_PASSWORD_FILE)
+export PG_PASSWORD=$(cat $POSTGRES_PASSWORD_FILE)
 export GAIA_X_API_KEY=$(cat $GAIA_X_API_KEY_FILE)
+export USGS_USER=$(cat $USGS_USER_FILE)
+export USGS_PASSWORD=$(cat $USGS_PASSWORD_FILE)
 
 tcpserver 0.0.0.0 $GAIA_OSGEO_API_PORT ./api.sh
