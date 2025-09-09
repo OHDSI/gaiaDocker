3fc8eb806a28f936525707ccd55b72647e7f7313
Timothy Norris <tibben@ocf.berkeley.edu>
2025-09-08 21:29:41 -0400

    feat: refactor PGPASSWORD env variable

--- docker/gaia-osgeo/init.sh
+++ docker/gaia-osgeo/init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export PG_PASSWORD=$(cat $POSTGRES_PASSWORD_FILE)
+echo gaia-db:$POSTGRES_PORT:$POSTGRES_DB:$POSTGRES_USER:$(cat $POSTGRES_PASSWORD_FILE) > ~/.pgpass
+chmod 0600 ~/.pgpass
+export PGPASSWORD=$(cat $POSTGRES_PASSWORD_FILE)
 export GAIA_X_API_KEY=$(cat $GAIA_X_API_KEY_FILE)
 export USGS_USER=$(cat $USGS_USER_FILE)
 export USGS_PASSWORD=$(cat $USGS_PASSWORD_FILE)
--- docker/gaia-postgis/init.sh
+++ docker/gaia-postgis/init.sh
@@ -2,6 +2,7 @@
 
 echo gaia-db:$POSTGRES_PORT:$POSTGRES_DB:$POSTGRES_USER:$(cat $POSTGRES_PASSWORD_FILE) > ~/.pgpass
 chmod 0600 ~/.pgpass
+export PGPASSWORD=$(cat $POSTGRES_PASSWORD_FILE)
 export GAIA_X_API_KEY=$(cat $GAIA_X_API_KEY_FILE)
 
 tcpserver 0.0.0.0 $GAIA_POSTGIS_API_PORT ./api.sh
