96dffdd3105099292e30a21673e9d4ebb907de46
Tim Norris <tibben@ocf.berkeley.edu>
2026-05-23 22:43:22 -0400

    feat: gaiaDocker works with gaiaDB consolidate pull request

--- docker/gaia-git/init.sh
+++ docker/gaia-git/init.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 export GAIA_X_API_KEY=$(cat $GAIA_X_API_KEY_FILE)
-tcpserver 0.0.0.0 $GAIA_GIT_API_PORT ./api.sh
+
+exec "$@"
--- docker/gaia-osgeo/init.sh
+++ docker/gaia-osgeo/init.sh
@@ -1,14 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
 echo gaia-db:$POSTGRES_PORT:$POSTGRES_DB:$POSTGRES_USER:$(cat $POSTGRES_PASSWORD_FILE) > ~/.pgpass
 chmod 0600 ~/.pgpass
-export PGPASSWORD=$(cat $POSTGRES_PASSWORD_FILE)
+export POSTGRES_PASSWORD=$(cat $POSTGRES_PASSWORD_FILE)
 export GAIA_X_API_KEY=$(cat $GAIA_X_API_KEY_FILE)
+echo [gaia-osgeo] api and postgis credentials set
 
 export USGS_USER=$(cat $USGS_USER_FILE)
 export USGS_PASSWORD=$(cat $USGS_PASSWORD_FILE)
 echo "machine urs.earthdata.nasa.gov login $USGS_USER password $USGS_PASSWORD" > ~/.netrc
 chmod 0600 ~/.netrc
 touch ~/.urs_cookies
+echo [gaia-osgeo] USGS credentials set
 
-tcpserver 0.0.0.0 $GAIA_OSGEO_API_PORT ./api.sh
+exec "$@"
