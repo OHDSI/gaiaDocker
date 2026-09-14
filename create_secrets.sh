15674281fe3c7dbbffdaeb25f6cf381e9f562dbc
Tim Norris <tibben@ocf.berkeley.edu>
2026-09-14 15:50:52 -0400

    debug: postgrest container on mac silicon

---
+++ create_secrets.sh
@@ -0,0 +1,8 @@
+if [ ! -f ./secrets/github_pat ]; then touch ./secrets/github_pat; fi
+if [ ! -f ./secrets/gaia/AIRNOW_API_KEY ]; then touch ./secrets/gaia/AIRNOW_API_KEY; fi
+if [ ! -f ./secrets/gaia/CDC_APP_TOKEN ]; then touch ./secrets/gaia/CDC_APP_TOKEN; fi
+if [ ! -f ./secrets/gaia/CENSUS_API_KEY ]; then touch ./secrets/gaia/CENSUS_API_KEY; fi
+if [ ! -f ./secrets/gaia/COPERNICUS_KEY ]; then touch ./secrets/gaia/COPERNICUS_KEY; fi
+if [ ! -f ./secrets/github_patgaia/USGS_PASSWORD ]; then touch ./secrets/gaia/USGS_PASSWORD; fi
+if [ ! -f ./secrets/github_patgaia/USGS_USER ]; then touch ./secrets/gaia/USGS_USER; fi
+echo all secrets checked and created as empty if none found
