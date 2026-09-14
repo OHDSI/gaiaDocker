d5411ec8d2944a1b016733909cc14790b0af3651
Tim Norris <tibben@ocf.berkeley.edu>
2026-09-14 16:42:08 -0400

    debug: create_secrets.ps1 ItemType

--- create_secrets.ps1
+++ create_secrets.ps1
@@ -4,25 +4,25 @@
 # NOTE: this will not overwrite existing secrets
 
 if (-not (Test-Path ".\secrets\github_pat")) {
-  New-Item -Path ".\secrets\github_pat" -Item_type File
+  New-Item -Path ".\secrets\github_pat" -ItemType File
 }
 if (-not (Test-Path ".\secrets\gaia\AIRNOW_API_KEY")) {
-  New-Item -Path ".\secrets\gaia\AIRNOW_API_KEY" -Item_type File
+  New-Item -Path ".\secrets\gaia\AIRNOW_API_KEY" -ItemType File
 }
 if (-not (Test-Path ".\secrets\gaia\CDC_APP_TOKEN")) {
-  New-Item -Path ".\secrets\gaia\CDC_APP_TOKEN" -Item_type File
+  New-Item -Path ".\secrets\gaia\CDC_APP_TOKEN" -ItemType File
 }
 if (-not (Test-Path ".\secrets\gaia\CENSUS_API_KEY")) {
-  New-Item -Path ".\secrets\gaia\CENSUS_API_KEY" -Item_type File
+  New-Item -Path ".\secrets\gaia\CENSUS_API_KEY" -ItemType File
 }
 if (-not (Test-Path ".\secrets\gaia\COPERNICUS_KEY")) {
-  New-Item -Path ".\secrets\gaia\COPERNICUS_KEY" -Item_type File
+  New-Item -Path ".\secrets\gaia\COPERNICUS_KEY" -ItemType File
 }
-if (-not (Test-Path ".\secrets\github_patgaia\USGS_PASSWORD")) {
-  New-Item -Path ".\secrets\gaia\USGS_PASSWORD" -Item_type File
+if (-not (Test-Path ".\secrets\gaia\USGS_PASSWORD")) {
+  New-Item -Path ".\secrets\gaia\USGS_PASSWORD" -ItemType File
 }
-if (-not (Test-Path ".\secrets\github_patgaia\USGS_USER")) {
-  New-Item -Path ".\secrets\gaia\USGS_USER" -Item_type File
+if (-not (Test-Path ".\secrets\gaia\USGS_USER")) {
+  New-Item -Path ".\secrets\gaia\USGS_USER" -ItemType File
 }
 
-Write-Host "all secrets checked and created as empty if none found"
+Write-Host "all secrets checked and created as empty if none found"Item_type
